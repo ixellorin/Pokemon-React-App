e9aca561d5cde4b65001d87dee73e684af919900
iellorin <iellorin@hotmail.com>
2020-04-27 14:20:52 -0700

    Implement search on add pokemon

--- pokemon-app/src/js/AddPokemon.js
+++ pokemon-app/src/js/AddPokemon.js
@@ -14,13 +14,49 @@ class AddPokemon extends React.Component {
 
   constructor(props) {
       super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+          filtered: props.listOfPokemon
+      }
   }
+
+  handleChange(e) {
+          // Variable to hold the original version of the list
+      let currentList = [];
+          // Variable to hold the filtered list before putting into state
+      let newList = [];
+
+          // If the search bar isn't empty
+      if (e.target.value !== "") {
+              // Assign the original list to currentList
+        currentList = this.props.listOfPokemon;
+
+              // Use .filter() to determine which items should be displayed
+              // based on the search terms
+        newList = currentList.filter(item => {
+                  // change current item to lowercase
+          const lc = item.name.toLowerCase();
+                  // change search term to lowercase
+          const filter = e.target.value.toLowerCase();
+                  // check to see if the current list item includes the search term
+                  // If it does, it will be added to newList. Using lowercase eliminates
+                  // issues with capitalization in search terms and search content
+          return lc.includes(filter);
+        });
+      } else {
+              // If the search bar is empty, set newList to original task list
+        newList = this.props.listOfPokemon;
+      }
+          // Set the filtered state based on what our rules added to newList
+      this.setState({
+        filtered: newList
+      });
+    }
+
   render() {
 
-    var pokemon = this.props.listOfPokemon;
-
     var onClick = this.props.addPokemon;
-    var buttonsList = pokemon.map(function(pokemon) {
+    var buttonsList = this.state.filtered.map(function(pokemon) {
       return <button onClick={() => onClick(pokemon.name, pokemon.id)} className={addPokemonButton}>
           <img className="add-pokemon-entry-image" src={myPokemonImages[pokemon.id]} />
             <span className="add-pokemon-entry-text">
@@ -33,6 +69,9 @@ class AddPokemon extends React.Component {
       <div className="add-pokemon-dialog" id="add-pokemon-dialog">
       <div className="add-pokemon-header">
         <h3 className="add-pokemon-header">ADD POK&eacute;MON</h3>
+      </div>
+      <div className="search-container">
+        <input type="text" className="add-input" onChange={this.handleChange} placeholder="Search..." />
       </div>
         <ul className="add-pokemon-list">
           {buttonsList}
